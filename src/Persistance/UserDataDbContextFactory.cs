f57958bc3457b996c8718bb2ee74276a058e94f7
Marcin Miazga <miazgamarcin@outlook.com>
2019-01-05 13:55:27 +0100

    add db context factory for the local create migrations feature

---
+++ src/Persistance/UserDataDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Outloud.ScoreService.Persistance
+{
+    public class UserDataDbContextFactory : IDesignTimeDbContextFactory<UserDataDbContext>
+    {
+        public UserDataDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<UserDataDbContext>();
+            optionsBuilder.UseSqlServer("Server=localhost,1434;Database=master;User=sa;Password=1qazxsW@;");
+
+            return new UserDataDbContext(optionsBuilder.Options);
+        }
+    }
+}
