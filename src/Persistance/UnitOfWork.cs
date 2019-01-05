b50c04450e09e965b9100e3bf604ed611862c823
Marcin Miazga <miazgamarcin@outlook.com>
2019-01-05 13:56:24 +0100

    create the unit of work

---
+++ src/Persistance/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+
+namespace Outloud.ScoreService.Persistance
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly UserDataDbContext context;
+
+        public UnitOfWork(UserDataDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task CompleteAsync()
+        {
+            await context.SaveChangesAsync();
+        }
+    }
+
+    public interface IUnitOfWork
+    {
+        Task CompleteAsync();
+    }
+}
